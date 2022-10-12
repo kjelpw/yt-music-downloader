3beafe7ba2852fd90cc29b0d954647f76e4ce3d3
kjelpw <kjelpw@protonmail.com>
2022-10-11 19:59:18 -0700

    main magic method

--- download.py
+++ download.py
@@ -1,3 +1,7 @@
+playliststart = 1
+playlistend = 24
+download_link = ''
+
 def guess_name():
     artist = ''
     title = ''
@@ -31,75 +35,77 @@ def eval_answer(answer, artist, title):
     return artist, title
 
 
-import os
-if not os.path.exists('music'):
-    os.mkdir('music')
-os.chdir('music')
-# download mp3
-import youtube_dl
+if __name__ == '__main__':
+    import os
+    if not os.path.exists('music'):
+        os.mkdir('music')
+    os.chdir('music')
+    # download mp3
+    import youtube_dl
 
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
+    def my_hook(d):
+        if d['status'] == 'finished':
+            print('Done downloading, now converting ...')
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    'playlistend': 213,
-    'skip_download': False,
-    'progress_hooks': [my_hook],
-    'writethumbnail': True,
-    'ignoreerrors': True
-}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([''])
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'playliststart': playliststart,
+        'playlistend': playlistend,
+        'skip_download': False,
+        'progress_hooks': [my_hook],
+        'writethumbnail': True,
+        'ignoreerrors': True
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([''])
 
-# add the thumbnails
-import eyed3
-import glob
+    # add the thumbnails
+    import eyed3
+    import glob
 
-print('\n\n\nmp3 editing')
+    print('\n\n\nmp3 editing')
 
-# for every file
-for file in glob.glob("*.mp3"):
-    print(file)
-    # load mp3
-    audiofile = eyed3.load(file)
-    if (audiofile.tag == None):
-        audiofile.initTag()
+    # for every file
+    for file in glob.glob("*.mp3"):
+        print(file)
+        # load mp3
+        audiofile = eyed3.load(file)
+        if (audiofile.tag == None):
+            audiofile.initTag()
 
-    print('thumbnail')
+        print('thumbnail')
 
-    # if .webp format exists, must convert
-    if os.path.exists(file[:-4] + '.webp'):
-        image_name = file[:-4] + '.webp'
-        #convert thumbnails to jpeg
-        from PIL import Image
-        im = Image.open(image_name).convert('RGB')
-        im.save(image_name[:-5] + '.jpg', 'jpeg')
-    # save jpg to mp3
-    if os.path.exists(file[:-4] + '.jpg'):
-        image_file = open(file[:-4] + '.jpg', 'rb').read()
-        audiofile.tag.images.set(3, image_file, 'image/jpeg')
-    else:
-        print(f'\n\n\n\n\n\n\nNO THUMBNAIL for {file}')
+        # if .webp format exists, must convert
+        if os.path.exists(file[:-4] + '.webp'):
+            image_name = file[:-4] + '.webp'
+            #convert thumbnails to jpeg
+            from PIL import Image
+            im = Image.open(image_name).convert('RGB')
+            im.save(image_name[:-5] + '.jpg', 'jpeg')
+        # save jpg to mp3
+        if os.path.exists(file[:-4] + '.jpg'):
+            image_file = open(file[:-4] + '.jpg', 'rb').read()
+            audiofile.tag.images.set(3, image_file, 'image/jpeg')
+        else:
+            print(f'\n\n\n\n\n\n\nNO THUMBNAIL for {file}')
 
-    #generate artist title
-    artist, title = guess_name()
-    audiofile.tag.title = title
-    audiofile.tag.artist = artist
+        #generate artist title
+        artist, title = guess_name()
+        audiofile.tag.title = title
+        audiofile.tag.artist = artist
 
-    audiofile.tag.save()
-    print('\n\n\n')
+        audiofile.tag.save()
+        print('\n\n\n')
 
-print('\n\n\n\n\nStarting Cleanup')
+    print('\n\n\n\n\nStarting Cleanup')
 
-for item in os.listdir('.'):
-    if item.endswith(".jpg") or item.endswith(".webp"):
-        print(f'Deleting: {item}')
-        os.remove(item)
-os.chdir('..')
+    for item in os.listdir('.'):
+        if item.endswith(".jpg") or item.endswith(".webp"):
+            print(f'Deleting: {item}')
+            os.remove(item)
+    os.chdir('..')
